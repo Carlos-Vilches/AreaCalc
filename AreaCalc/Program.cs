8763e98d782a63959b5d9f03c99a3ebf6069cdbd
Carlos-Vilches <146343088+Carlos-Vilches@users.noreply.github.com>
2023-12-11 13:00:34 +0100

    Update Program.cs

--- AreaCalc/Program.cs
+++ AreaCalc/Program.cs
@@ -7,11 +7,10 @@ namespace areaCalc
         static void Main()
         {
             const string MsgChooseFigure = "De quina figura vols calcular l'area?\n a. quadrat\n b. rectangle\n c. cercle\n d. pentagon";
-            const string MsgInputBase = "Introdueix la base de la figura: ";
-            const string MsgInputHeight = "Introdueix la altura: ";
+            const string MsgInputBase = "Introdueix la base de la figura";
             const string MsgArea = "Area: ";
 
-            int area, baseFig, height;
+            int area, baseFig;
             char figure;
 
             Console.WriteLine(MsgChooseFigure);
@@ -26,24 +25,11 @@ namespace areaCalc
                     area = Calc(baseFig);
                     Console.WriteLine(MsgArea + area);
                     break;
-
-                case 'b':
-                    Console.WriteLine(MsgInputBase);
-                    baseFig = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(MsgInputHeight);
-                    height = Convert.ToInt32(Console.ReadLine());
-                    area = Calc(baseFig, height);
-                    Console.WriteLine(MsgArea + area);
-                    break;
             }
         }
-        public static int Calc(int baseFig)
+        public static int Calc(int num)
         {
-            return baseFig * baseFig;
-        }
-        public static int Calc(int baseFig, int height)
-        {
-            return baseFig * height;
+            return num * num;
         }
     }
 }
