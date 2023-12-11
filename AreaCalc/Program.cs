a125db5546fc26fff54ba0dc7179aa1d7544555c
Carlos-Vilches <carlos.vilches.7e7@itb.cat>
2023-12-11 13:02:30 +0100

    Calculate rectangle area. Closes #2

--- AreaCalc/Program.cs
+++ AreaCalc/Program.cs
@@ -1,1 +1,49 @@
+using System;
 
+namespace areaCalc
+{
+    public class areaCalc
+    {
+        static void Main()
+        {
+            const string MsgChooseFigure = "De quina figura vols calcular l'area?\n a. quadrat\n b. rectangle\n c. cercle\n d. pentagon";
+            const string MsgInputBase = "Introdueix la base de la figura: ";
+            const string MsgInputHeight = "Introdueix la altura: ";
+            const string MsgArea = "Area: ";
+
+            int area, baseFig, height;
+            char figure;
+
+            Console.WriteLine(MsgChooseFigure);
+            figure = Convert.ToChar(Console.ReadLine());
+            figure = char.ToLower(figure);
+
+            switch (figure)
+            {
+                case 'a':
+                    Console.WriteLine(MsgInputBase);
+                    baseFig = Convert.ToInt32(Console.ReadLine());
+                    area = Calc(baseFig);
+                    Console.WriteLine(MsgArea + area);
+                    break;
+
+                case 'b':
+                    Console.WriteLine(MsgInputBase);
+                    baseFig = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(MsgInputHeight);
+                    height = Convert.ToInt32(Console.ReadLine());
+                    area = Calc(baseFig, height);
+                    Console.WriteLine(MsgArea + area);
+                    break;
+            }
+        }
+        public static int Calc(int baseFig)
+        {
+            return baseFig * baseFig;
+        }
+        public static int Calc(int baseFig, int height)
+        {
+            return baseFig * height;
+        }
+    }
+}
