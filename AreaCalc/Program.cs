c7ffc3a8aa2ca9beb19f079faade7b6c6c9bde71
Carlos-Vilches <carlos.vilches.7e7@itb.cat>
2023-12-11 13:54:42 +0100

    calculate pentgon area. Closes #4

--- AreaCalc/Program.cs
+++ AreaCalc/Program.cs
@@ -8,16 +8,17 @@ namespace areaCalc
         {
             const string MsgChooseFigure = "De quina figura vols calcular l'area?\n a. quadrat\n b. rectangle\n c. cercle\n d. pentagon";
             const string MsgInputBase = "Introdueix la base de la figura: ";
+            const string MsgInputRadius = "Introdueix el radi del cercle: ";
+            const string MsgInputApothem = "Introdueix l'apotema: ";
             const string MsgInputHeight = "Introdueix la altura: ";
             const string MsgArea = "Area: ";
+            const double PI = 3.1415926535897931;
 
-            int area, baseFig, height;
-
-            const string MsgInputBase = "Introdueix la base de la figura";
-            const string MsgArea = "Area: ";
-
-            int area, baseFig;
             char figure;
+
+            float area, apothem;
+
+            int baseFig, height;
 
             Console.WriteLine(MsgChooseFigure);
             figure = Convert.ToChar(Console.ReadLine());
@@ -40,6 +41,22 @@ namespace areaCalc
                     area = Calc(baseFig, height);
                     Console.WriteLine(MsgArea + area);
                     break;
+
+                case 'c':
+                    Console.WriteLine(MsgInputRadius);
+                    baseFig = Convert.ToInt32(Console.ReadLine());
+                    area = Calc(baseFig, PI);
+                    Console.WriteLine(MsgArea + area);
+                    break;
+
+                case 'd':
+                    Console.WriteLine(MsgInputBase);
+                    baseFig = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(MsgInputApothem);
+                    apothem = Convert.ToSingle(Console.ReadLine());
+                    area = Calc(baseFig, apothem);
+                    Console.WriteLine(MsgArea + area);
+                    break;
             }
         }
         public static int Calc(int baseFig)
@@ -49,6 +66,16 @@ namespace areaCalc
         public static int Calc(int baseFig, int height)
         {
             return baseFig * height;
+        }
+        public static float Calc(int radius, double PI)
+        {
+            return (float)(PI * (radius * radius));
+        }
+        public static float Calc(int baseFig, float apothem)
+        {
+            int sides = 5, Two = 2;
+            int perim = baseFig * sides;
+            return perim * apothem / Two;
         }
     }
 }
